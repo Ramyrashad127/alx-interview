250b6f5d72625ba4072db3beef76d939a391140c
Ramyrashad127 <ramy41599@gmail.com>
2024-08-11 20:45:26 +0300

    new

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    
+    l = [[1]]
+    
+    for i in range(1, n):
+        prev = [1]
+        for j in range(1, i):
+            prev.append(l[i-1][j-1] + l[i-1][j])
+        prev.append(1)
+        l.append(prev)
+    
+    return l
