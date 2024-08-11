6fb1b1c2f034eb0eb060f86d7f5a53d208946a88
Ramyrashad127 <ramy41599@gmail.com>
2024-08-11 10:54:17 -0700

    new

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,14 +8,14 @@ def pascal_triangle(n):
     """ new function"""
     if n <= 0:
         return []
-    
-    l = [[1]]
-    
+
+    li = [[1]]
+
     for i in range(1, n):
         prev = [1]
         for j in range(1, i):
-            prev.append(l[i-1][j-1] + l[i-1][j])
+            prev.append(li[i-1][j-1] + li[i-1][j])
         prev.append(1)
-        l.append(prev)
-    
-    return l
+        li.append(prev)
+
+    return li
