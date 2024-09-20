fbf019f4827da10e74d200594b25a9fd33e8d519
Ramyrashad127 <ramy41599@gmail.com>
2024-09-20 22:57:50 +0300

    new

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,18 +3,20 @@
 const fetch = require('node-fetch');
 const movieId = process.argv[2];
 
-
 const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-
 (async () => {
-    const response = await fetch(apiUrl);
-    const movieData = await response.json();
-    const characterUrls = movieData.characters;
+    try {
+        const response = await fetch(apiUrl);
+        const movieData = await response.json();
+        const characterUrls = movieData.characters;
 
-    for (const url of characterUrls) {
+        for (const url of characterUrls) {
             const charResponse = await fetch(url);
             const charData = await charResponse.json();
             console.log(charData.name);
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
     }
 })();
