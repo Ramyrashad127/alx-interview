946bc11fc519865a4b583fc66517170b945e8510
Ramyrashad127 <ramy41599@gmail.com>
2024-09-20 23:12:50 +0300

    ne

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,11 +6,18 @@ const movieId = process.argv[2];
 const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
 request(apiUrl, (error, response, body) => {
+    if (error) {
+        throw error
+    }
     const movieData = JSON.parse(body);
     const characterUrls = movieData.characters;
 
     characterUrls.forEach((url) => {
         request(url, (charError, charResponse, charBody) => {
+            if (charError) {
+                throw charError;
+            }
+
             const characterData = JSON.parse(charBody);
             console.log(characterData.name);
         });
