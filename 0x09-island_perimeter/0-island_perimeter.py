ddf1c6049845dcc472bee36b6320af9bb58b4c4e
Ramyrashad127 <ramy41599@gmail.com>
2024-10-09 00:17:35 +0300

    new

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""island_perimeter"""
+
+
+def island_perimeter(grid):
+    """island_perimeter"""
+    cnt = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if(j-1 >= 0 and grid[i][j-1] == 0):
+                    cnt += 1
+                if (j+1 < len(grid[i]) and grid[i][j+1] == 0):
+                    cnt += 1
+                if (i-1 >= 0 and grid[i-1][j] == 0):
+                    cnt += 1
+                if (i+1 < len(grid) and grid[i+1][j] == 0):
+                    cnt += 1
+    return cnt
