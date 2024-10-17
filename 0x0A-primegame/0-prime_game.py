acb0b81624b4fd2e5bc5d48142b05bc8376499f4
Ramyrashad127 <ramy41599@gmail.com>
2024-10-17 11:39:04 +0300

    new

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,17 +6,20 @@ def isWinner(x, nums):
     """Prime Game function ALX"""
     Maria = 0
     Ben = 0
-    prime = [0, 0]
-    for i in range(2, 10005):
-        prime.append(1)
+    limit = max(nums)
+    prime = [0] * (limit + 1)
 
-    for i in range(2, 10001):
+    for i in range(2, limit + 1):
+        prime[i] = 1
+
+    for i in range(2, int(limit ** 0.5) + 1):
         if prime[i] == 1:
-            for j in range(i + i, 10001, i):
+            for j in range(i * i, limit + 1, i):
                 prime[j] = 0
 
-    for i in range(1, 10001):
+    for i in range(1, limit + 1):
         prime[i] += prime[i - 1]
+
     for n in nums:
         if prime[n] % 2 == 0:
             Ben += 1
