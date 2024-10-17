96c694a28bd53c0decf11419008e698b95da597b
Ramyrashad127 <ramy41599@gmail.com>
2024-10-17 11:15:26 +0300

    new

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,24 +6,26 @@ def isWinner(x, nums):
     """Prime Game"""
     Maria = 0
     Ben = 0
-    prime  = [1]
+    prime = [0, 0]
     for i in range(2, 10005):
         prime.append(1)
+
     for i in range(2, 10001):
-        if prime[i]:
-            for j in range(i*i, 10001, i):
+        if prime[i] == 1:
+            for j in range(i * i, 10001, i):
                 prime[j] = 0
-    for i in range(2, 10001):
+
+    for i in range(1, 10001):
         prime[i] += prime[i-1]
+
     for n in nums:
-        if prime[n] % 2 != 0:
+        if prime[n] % 2 == 0:
             Ben += 1
         else:
             Maria += 1
+
     if Maria < Ben:
         return "Ben"
     if Maria > Ben:
         return "Maria"
     return None
-
-    
